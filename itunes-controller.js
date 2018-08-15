8fa3b242d3c985906866c58ffe38efc0894e658a
Ryan Gaige <Ryan@fencepencil.com>
2018-08-14 20:55:22 -0600

    draw to dom

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,30 +1,49 @@
 function ItunesController() {
-
   // PRIVATE
-  const itunesService = new ItunesService()
+  const itunesService = new ItunesService();
 
-  function drawSongs(results) {
-    console.log(results)
+  function drawSongs(songs) {
     //YOUR CODING STARTS HERE
+    let template = "";
 
+    for (let i = 0; i < songs.length; i++) {
+      const song = songs[i];
+      template += `
 
+      <div class="container-fluid">
+         <div class="row">
+            <div class="col-3">
+            <p>Title: ${song.title}</p>
+            <img src="${song.albumArt}" alt="">
+            <p>Artist: ${song.artist}</p>
+            <p>Collection: ${song.collection}</p>
+            <p>price: ${song.price}</p> 
+            <audio controls><source src="${song.preview}" type="audio/mpeg">
+            </audio>
 
+          </div>
+        </div>
+    </div>
+
+        `;
+    }
+    document.getElementById("songs").innerHTML = template;
+
+    console.log(songs);
   }
+  getMusicByArtist.reset();
 
   //PUBLIC
   //DO NOT MODIFY THIS METHOD
-  this.getMusic = function (e) {
+  this.getMusic = function(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     //changes the button to loading while songs load
-    $('#get-music-button').text('LOADING....');
+    $("#get-music-button").text("LOADING....");
     itunesService.getMusicByArtist(artist).then(results => {
-      drawSongs(results)
+      drawSongs(results);
       //changes button back to GET MUSIC once songs are loaded
-      $('#get-music-button').text('GET MUSIC');
-    })
-  }
-
-
+      $("#get-music-button").text("GET MUSIC");
+    });
+  };
 }
-
--- itunes-service.js
+++ itunes-service.js
@@ -1,10 +1,12 @@
 function ItunesService() {
-
   //DO NOT MODIFY
-  this.getMusicByArtist = function (artist) {
-    var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
-    //Casts each object to 
-    return $.getJSON(url).then(function (response) {
+  this.getMusicByArtist = function(artist) {
+    let url1 = "//bcw-getter.herokuapp.com/?url=";
+    let url2 = "https://itunes.apple.com/search?term=" + artist;
+    let apiUrl = url1 + encodeURI(url2);
+
+    //Casts each object to
+    return $.getJSON(apiUrl).then(function(response) {
       var songList = response.results.map(song => {
         return {
           title: song.trackName,
@@ -12,10 +14,28 @@ function ItunesService() {
           artist: song.artistName,
           collection: song.collectionName,
           price: song.collectionPrice,
-          preview: song.previewUrl,
-        }
-      })
+          preview: song.previewUrl
+        };
+      });
       return songList;
-    })
-  }
+    });
+  };
+
+  // this.getMusicByArtist = function (artist) {
+  //   var url = 'https://itunes.apple.com/search?term=' + artist + "&callback=?"
+  //   //Casts each object to
+  //   return $.getJSON(url).then(function (response) {
+  //     var songList = response.results.map(song => {
+  //       return {
+  //         title: song.trackName,
+  //         albumArt: song.artworkUrl60.replace(/60x60/g, "250x250"),
+  //         artist: song.artistName,
+  //         collection: song.collectionName,
+  //         price: song.collectionPrice,
+  //         preview: song.previewUrl,
+  //       }
+  //     })
+  //     return songList;
+  //   })
+  // }
 }
